9a25e30c7ebd92977b02b4286ab70428c9a3b03a
MrKrabat <laurent.skiba@web.de>
2016-10-08 14:27:22 +0200

    Initial release
    Initial release

---
+++ README.md
@@ -0,0 +1,16 @@
+URLReceiver - Send URLs for playback directly on your Kodi device!
+Service Addon for Kodi v16 Jarvis
+
+Required to have a standalone client for sending URLs.
+Optionally it is offering a web interface.
+
+==========
+
+
+Website: https://dev.mrkrabat.de/urlreceiver/
+
+Git Repo: https://github.com/MrKrabat/service.urlreceiver
+
+Forum Posting: Not yet
+
+URL Sender: https://github.com/MrKrabat/URLSender
---
+++ default.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+# URLReceiver - Send URLs for playback directly on your Kodi device.
+# Copyright (C) 2016  MrKrabat
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Imports
+import sys, socket
+import xbmc, xbmcaddon, xbmcgui
+import urlresolver
+
+# Constants
+__addonid__	  = 'service.urlreceiver'
+__settings__  = xbmcaddon.Addon(id=__addonid__)
+__plugin__    = __settings__.getAddonInfo('name')
+__version__   = __settings__.getAddonInfo('version')
+__path__	  = __settings__.getAddonInfo('path')
+__port__ 	  = __settings__.getSetting('port')
+
+
+# function to send messages
+def sendtoclient(socket, browser, message):
+	if not browser:
+		socket.send(message)
+	else:
+		socket.sendall('HTTP/1.1 200 OK\nContent-Type: text/html\nAccess-Control-Allow-Origin: *\n\n' + message)
+
+
+# start program
+if __name__ == '__main__':
+	monitor = xbmc.Monitor()
+	# get playable extensions
+	__mediaext__  = tuple(str(xbmc.getSupportedMedia('video') + "|" + xbmc.getSupportedMedia('music')).split('|'))
+	# read URLSender.html
+	with open(__path__ + '/urlsender.html', 'r') as myfile:
+		__urlsender__ = myfile.read()
+	
+	try:
+		# create listening socket
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.bind((xbmc.getIPAddress(), int(__port__)))
+		sock.listen(1)
+		sock.setblocking(0)
+		xbmc.log("[SERVICE] %s: Initializing version %s on port %s" % (__plugin__, __version__, __port__), xbmc.LOGDEBUG)
+		xbmcgui.Dialog().notification(__plugin__, __settings__.getLocalizedString(30100), xbmcgui.NOTIFICATION_INFO)
+	except socket.error:
+		# unable to listen on port
+		xbmc.log("[SERVICE] %s: Initializing version %s failed on port %s" % (__plugin__, __version__, __port__), xbmc.LOGFATAL)
+		xbmcgui.Dialog().notification(__plugin__, __settings__.getLocalizedString(30101) % __port__, xbmcgui.NOTIFICATION_ERROR)
+		sys.exit()
+	
+	# main loop
+	while not monitor.abortRequested():
+		# shutdown requested?
+		if monitor.waitForAbort(0.20):
+			break
+		
+		try:
+			# wait for incoming connection
+			connection, client_address = sock.accept()
+			try:
+				# receive message
+				connection.settimeout(10.0)
+				data = connection.recv(4096).rstrip()
+				if data:
+					# test if client is browser
+					browser = False
+					if xbmc.getIPAddress() in data:
+						data = data.split('\n', 1)[0]
+						data = data[13:-9].strip()
+						browser = True
+						if not data or data == "ico":
+							# send webinterface
+							sendtoclient(connection, browser, __urlsender__)
+							continue
+					
+					# Debug: log URL
+					xbmc.log("[SERVICE] %s: URL received: %s" % (__plugin__, data), xbmc.LOGDEBUG)
+					
+					# test if link is video or need to be resolved
+					link = ''
+					if data.lower().endswith(__mediaext__):
+						link = data
+					elif data:
+						try:
+							link = urlresolver.resolve(data)
+						except:
+							pass
+					
+					if link:
+						if xbmc.Player().isPlaying():
+							# add to playlist
+							sendtoclient(connection, browser, '2')
+							playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+							playlist.add(link)
+						else:
+							# play
+							sendtoclient(connection, browser, '1')
+							playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+							playlist.clear()
+							playlist.add(link)
+							xbmc.Player().play(playlist)
+					
+					else:
+						# unable to play
+						sendtoclient(connection, browser, '0')
+						xbmc.log("[SERVICE] %s: The received URL could not be played" % __plugin__, xbmc.LOGDEBUG)
+						xbmcgui.Dialog().notification(__plugin__, __settings__.getLocalizedString(30102), xbmcgui.NOTIFICATION_WARNING)
+			
+			finally:
+				# close connection
+				connection.close()
+		
+		except socket.error:
+			# continue if no connection
+			continue
+	
+	
+	# shut down service
+	xbmc.log("[SERVICE] %s: The service will be shut down" % __plugin__, xbmc.LOGDEBUG)
+	sock.close()
